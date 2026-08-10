b5bd4792d95130d0334e6489cd5b75347487d363
arunratsat90-droid <arunrat.sat90@esdc.go.th>
2026-08-10 14:33:23 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/PLC-STEAM/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
